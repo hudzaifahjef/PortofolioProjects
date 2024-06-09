46d93c6f130520af84830fbc3c0367d7072ed7b5
Hudzaifah-Jef <hudzaifah.jef@gmail.com>
2024-06-09 07:37:43 +0700

    Update Data Cleaning Project Query - layoff_dataset.sql

--- Data Cleaning Project Query - layoff_dataset.sql
+++ Data Cleaning Project Query - layoff_dataset.sql
@@ -1,9 +1,11 @@
+/*
+	
 # SQL-Cleaning-World-Layoff
 This data cleaning project use world-layoffs dataset. Project involves cleaning a dataset related to layoff statistics of businesses operating between 2020 and 2023. The cleaning process includes steps such as handling NULL values, removing duplicates, standardizing data formats, and applying statistical methods to improve data quality.
 
-
-    Select * 
-    From CleaningProject..layoffs
+Import data to SQL Server
+Create new databse
+Create new query
 
 -- Here are the steps
 
@@ -11,6 +13,12 @@ This data cleaning project use world-layoffs dataset. Project involves cleaning 
 	2. Standardize Value
 	3. Null Values or Blank Values
 	4. Remove any columns
+
+*/
+
+    Select * 
+    From CleaningProject..layoffs
+
 
 -- Create staging data table for backup or original references
 
